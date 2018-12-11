6b7a5c2e88dfdebf55537fbdd777cb7b494e2107
mjacobs <mjacobs@linux-n2cd>
2018-12-11 10:51:24 -0500

    strain gauge init working

--- motor.c
+++ motor.c
@@ -4,11 +4,18 @@
 
 
 
-#include "mcc_generated_files/pin_manager.h"
+#include "mcc_generated_files/mcc.h"
+#include "mcc_generated_files/system.h"
+
+
+#define FCY     30170937UL
+
+
+#include <libpic30.h>
 #include "stddef.h"
 #include "motor.h"
 
-
+ 
 
 
 
@@ -26,8 +33,66 @@ uint16_t motor_ReadLimits1(void){
     
 }
 
+void motor_MoveUptoLimit(void){
+    PWM_GENERATOR genNum = PWM_GENERATOR_1;
+    
+    PWM_OverrideLowDisable(genNum);   
+    
+    IO_RB10_SetHigh();
+    
+    while(IO_RA4_GetValue() == 0);
+    
+    PWM_OverrideLowEnable(genNum);
+    
+    __delay_ms(5);
+    
+    IO_RB10_SetLow();
+    
+}
+
+void motor_MoveDowntoLimit(void){
+    
+    PWM_GENERATOR genNum = PWM_GENERATOR_1;
+    
+    PWM_OverrideHighDisable(genNum);   
+    
+    IO_RB10_SetHigh();
+    
+    while(IO_RB8_GetValue() == 0);
+    
+    PWM_OverrideHighEnable(genNum);
+    
+    __delay_ms(5);
+    
+    IO_RB10_SetLow();     
+    
+}
+
+void motor_MoveUp(void){
+    
+    PWM_GENERATOR genNum = PWM_GENERATOR_1;
+    
+    PWM_OverrideLowDisable(genNum);   
+    
+    IO_RB10_SetHigh();        
+}
 
 
+void motor_MoveDown(void){
+    
+    PWM_GENERATOR genNum = PWM_GENERATOR_1;
+    
+    PWM_OverrideHighDisable(genNum);   
+    
+    IO_RB10_SetHigh();    
+}
+
+void motor_Hold(void){
+    PWM_GENERATOR genNum = PWM_GENERATOR_1;
+    
+    PWM_OverrideHighEnable(genNum);
+    PWM_OverrideLowEnable(genNum);
+}
 
 
 
